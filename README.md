2f7239c51bb5f655f1c09e89c3850d351791af70
Shyam <shyamsinghania4@gmail.com>
2020-12-01 22:32:44 +0530

    Add date section

---
+++ README.md
@@ -0,0 +1,4 @@
+Vehicle model to add vehcle details in db
+pettyCashbook model to keep track of balance in various places
+
+addBalance for updating balance of places when cash is received by them
--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-const express=require("express");
+const express = require("express");
 const app=express();
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const cors=require("cors");
-const cron=require('node-cron');
 
 app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser:true,useUnifiedTopology:true});
+//mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser:true,useUnifiedTopology:true});
 
-//mongoose.connect("mongodb://localhost:27017/VehicleDB",{ useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb://localhost:27017/SwastikDB",{ useNewUrlParser:true,useUnifiedTopology:true});
 
-const vehicleSchema=new mongoose.Schema({
-    Date:{
+const vehicles=new mongoose.Schema({
+    Date: {
         type:Number,
-        default: new Date().getDate()
+        default:new Date().getDate()
     },
     Month:{
         type:Number,
-        default: new Date().getMonth()+1
+        default:new Date().getMonth()+1
     },
     Year: {
         type:Number,
-        default: new Date().getFullYear()
+        default:new Date().getFullYear()
     },
     Vehicle : String,
+    From :String,
+    To :String,
     DIno : Number,
-    Place: String,
     Qty: Number,
     Rate: Number,
     RateCost: Number,
@@ -37,189 +37,135 @@ const vehicleSchema=new mongoose.Schema({
     DieselCost:Number,
     Balance: Number,
     Expenses: Number,
-    Remarks: String
+    Place:String,
+    Remarks: String,
+    Debit:Number // specific for petty cashbook
 })
 
-const PlaceVehSchema= new mongoose.Schema({
-    Date:{
-        type:Number,
-        default: new Date().getDate()
-    },
-    Month:{
-        type:Number,
-        default: new Date().getMonth()+1
-    },
-    Year: {
-        type:Number,
-        default: new Date().getFullYear()
-    },
-    Vehicle : String,
-    Credit: Number,
-    Debit : Number,
-    Remarks : String
+const pettyCashbooks=new mongoose.Schema({
+    Place:String,
+    Balance:Number
 })
 
-const placeSchema=new mongoose.Schema({
-    Place: String,
-    Balance : {
-        Current :{
-            type: Number,
-            default : 0
-        },
-        Daily:[{
-            Date:{
-                type:Number,
-                default: new Date().getDate()
-            },
-            Month:{
-                type:Number,
-                default: new Date().getMonth()
-            },
-            Year:{
-                type:Number,
-                default: new Date().getFullYear()
-            },
-            Open: Number,
-            Close: Number
-        }]
-    },
-    Payment : [PlaceVehSchema]
-})
-
-const Vmodel=mongoose.model("Vehicle",vehicleSchema); // model for Vehicle Details
-const PVmodel= mongoose.model("PlaceVeh",PlaceVehSchema); // model for vehicle took payment from place
-const Pmodel=mongoose.model("Place",placeSchema); // model for Place Details
-
-cron.schedule("0 0 0 * * *", function(x) { // to update opening and closing balance everyday
-Pmodel.find(function(err,found){
-    found.map(function(data){
-        var bal={
-            Open:data.Balance.Current,
-            Close:data.Balance.Current
-        };
-        data.Balance.Daily.push(bal);
-        data.save();
-    })
-})
-});
-
-function addVmodel(info){
-    var data=new Vmodel(info);
-    data.save();
-}
-
-function addPmodel(infos){
-    info=infos.Data;
-    Pmodel.find({'Place':infos.Place},function(err,found){
-        if(found.length==0){ // if not exist then create one
-            var data= new Pmodel({
-                Place: infos.Place,
-                Balance: {
-                    Current: -info.Credit,
-                    Daily:[{
-                        Date:info.Date,
-                        Month:info.Month,
-                        Year:info.Year,
-                        Close:-info.Credit,
-                        Open:0
-                    }]
-                },
-                Payment : [info]
-            })
-            data.save(function(err){
-                if(err) res.send(false);
-            })
-        }
-        else{ // if exist then update it
-        found[0].Payment.push(info);
-        found[0].Balance.Current-=info.Credit; // deduct credit from current
-        var prevClose=-info.Credit; // think to update closing balance
-        // Pmodel.find({'Place':info.Place}) // find the closing balance on that date
-        // .where('Balance.Daily')
-        // .elemMatch({'Date':info.Date,'Month':info.Month,'Year':info.Year})
-        // .select('Balance.Daily').exec(function(err,found){
-        //     prevClose+=found[0].Balance.Daily[0].Close;
-        // });
-        // Pmodel.findOneAndUpdate({'Place':info.Place,'Balance.Daily.Date':info.Date,'Balance.Daily.Month':info.Month,'Balance.Daily.Year':info.Year},
-        // {$set:{'Balance.Daily.$.Close':prevClose}}) // update the closing balance of that day
-        found[0].save()
-        }
-    })
-}
+const Vehicle=mongoose.model("Vehicle",vehicles);
+const PettyCashbook=mongoose.model("PettyCashbook",pettyCashbooks);
 
 app.get('/',function(req,res){
     res.send('App is Working');
 })
 
 app.get("/Vehicle",function(req,res){ // send all unique vehicle 
-    Vmodel.distinct("Vehicle",function(err,detail){
+    Vehicle.distinct("Vehicle",function(err,detail){
+        if(err){
+            res.status(404).send(false);
+        }
         res.send(detail);
     });
 })
 
 app.get("/Place",function(req,res){ //send all unique place
-    Pmodel.distinct("Place",function(err,data){
+    PettyCashbook.distinct("Place",function(err,data){
+        if(err){
+            res.send(false);
+        }
         res.send(data);
     })
 })
 
-app.get("/vehicle/:num",function(req,res){ // send particular vehicle details
-    const num=req.params.num;
-    Vmodel.find({"Vehicle":num},function(err,detail){
-        if(err){} // think what to do
+app.get("/Transactions",function(req,res){ // send particular vehicle details
+    Vehicle.find(function(err,detail){
+        if(err){
+            res.send(false);
+        }
         res.send(detail);
     })
 })
 
-app.get("/place/:place",function(req,res){
-    const place=req.params.place;
-    Pmodel.find({'Place':place},function(err,data){
-        if(err){} // think what to do
-        res.send(data);
+app.get("/Vehicle/:num",function(req,res){ // send particular vehicle details
+    const num=req.params.num;
+    Vehicle.find({"Vehicle":num},function(err,detail){
+        if(err){
+            res.send(false);
+        }
+        res.send(detail);
     })
 })
 
-app.post("/ComposeVehicle",function(req,res){ // post details of per vehicle loaded
-    addVmodel(req.body);
+app.get("/Place/:place",function(req,res){ // send particular place details
+    const place=req.params.place;
+    var detail={};
+    PettyCashbook.find({'Place':place},function(err,data){
+        if(err){
+            res.send(false);
+        }
+        detail.Balance=data[0].Balance;
+    })
+    Vehicle.find({'Place':place},function(err,data){
+        if(err){
+            res.send(false);
+        }
+        detail.detail=data;
+        res.send(detail);
+    })
+})
+
+app.post("/Compose",function(req,res){ // post vehciles data  
+    new Vehicle(req.body).save(function(err){
+        if(err) res.send(false);
+    });
     if(req.body.Expenses!==0){
-        const pData= new PVmodel(req.body);
-        pData.Credit=req.body.Expenses;
-        var detail={
-            Place:req.body.Place,
-            Data:pData
-        };
-        addPmodel(detail);
+        PettyCashbook.find({'Place':req.body.Place},function(err,found){ // if expenses then update current from where it is received
+            if(err) res.send(false);
+            else if(found.length===0){
+                new PettyCashbook({
+                    Place:req.body.Place,
+                    Balance :-req.body.Expenses
+                }).save(function(err){
+                    if(err) res.send(false);
+                });
+            }
+            else{
+                found[0].Balance-=req.body.Expenses;
+                found[0].save(function(err){
+                    if(err) res.send(false);
+                });
+            }
+            res.send(true);
+        })
     };
 })
 
-app.post("/ComposePlace",function(req,res){ // post deatils of payment from diff places
-    req.body.Expenses=req.body.Credit; // for vehicle section
-    addVmodel(req.body);
-    const data=new PVmodel(req.body);
-    var detail={
-        Place:req.body.Place,
-        Data:data
-    }
-    addPmodel(detail);
-})
-
-app.post("/addBalance",function(req,res){ // add balance to place
-    var place=req.body.place;
-    var bal=req.body.balance;
-    Pmodel.find({'Place':place},function(err,found){
-        found[0].Balance.Current+=bal; // update the current balance
-        var len=found[0].Balance.Daily.length;
-        found[0].Balance.Daily[len-1].Close+=bal; // update the closing balance // change it too
-        var data =new PVmodel({
-            Debit:bal,
-            Remarks:'Cash Received'
-        });
-        found[0].Payment.push(data); // add balance details
+app.post("/addBalance",function(req,res){ // add balance to corresponding place
+    var place=req.body.Place;
+    new Vehicle(req.body).save(function(err){
+        if(err) res.send(false);
+    });
+    PettyCashbook.find({'Place':place},function(err,found){
+        if(err) res.send(false);
+        found[0].Balance+=req.body.Debit;
         found[0].save(function(err){
             if(err) res.send(false);
-            else res.send(true);
         });
-    });
+    })
+})
+
+app.put("/editData",function(req,res){ // add diesel price and other details
+    var data=req.body;
+    function addData(Dkey,Dvalue){ 
+        Vehicle.find({'_id':Dkey},function(err,found){
+            if(err) res.send(false);
+            found[0].DieselPrice=Dvalue;
+            found[0].DieselCost=found[0].DieselPrice * found[0].Diesel;
+            found[0].Balance=found[0].RateCost-found[0].DieselCost-found[0].Expenses;
+            found[0].save(function(err){
+                if(err) res.send(false);
+            })
+        })
+    }
+    for(var key in data){
+        addData(key,data[key]);
+    }
+    res.send(true);
 })
 
 app.listen(process.env.PORT||5000,function(){
