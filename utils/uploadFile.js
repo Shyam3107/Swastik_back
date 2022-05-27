f68f2f9b1cdf0df0cb999aebeeb34e6c03d956bc
Shyam Singhania <shyamsinghania4@gmail.com>
2022-05-27 22:03:52 +0530

    Update packages

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,16 +1,16 @@
-import multer, { diskStorage } from "multer"
-import { join } from "path"
+// import multer, { diskStorage } from "multer"
+// import { join } from "path"
 
-const filePathNew = join(__dirname, "../public")
+// const filePathNew = join(__dirname, "../public")
 
-const storage = diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, filePathNew)
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`)
-  },
-})
+// const storage = diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, filePathNew)
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`)
+//   },
+// })
 
-const uploadFile = multer({ storage: storage })
-export default uploadFile
+// const uploadFile = multer({ storage: storage })
+// export default uploadFile
