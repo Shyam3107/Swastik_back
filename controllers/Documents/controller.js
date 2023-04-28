43e7c6496228b6ba5f8938b5cce6e90c45ce5930
Shyam Pat Singhania <shyamsinghania4@gmail.com>
2023-04-29 01:35:01 +0530

    Update Engines

--- controllers/Documents/controller.js
+++ controllers/Documents/controller.js
@@ -141,8 +141,8 @@ export const uploadDocuments = async (req, res) => {
       data.push(tempVal)
     }
 
+    console.log("Data : ", data)
     const insertData = await Document.insertMany(data, { session })
-
     await session.commitTransaction()
 
     return res.status(200).json({
